2079f063a467142e61221fe3f5b3983c767bd3e3
jloperad <51926858+jloperad@users.noreply.github.com>
2022-06-01 13:16:39 -0500

    fixed

--- API-tests/test/PraxisListItems.test.js
+++ API-tests/test/PraxisListItems.test.js
@@ -9,6 +9,10 @@ chai.use(jsonSchema);
 
 const PraxisURL = 'http://54.162.70.124:8081/api';
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 describe('Test List Items Endpoint', () => {
   beforeEach(async () => {
     const items = await agent.get(`${PraxisURL}/items`);
@@ -16,13 +20,13 @@ describe('Test List Items Endpoint', () => {
       items.body.forEach(async (item) => { await agent.delete(`${PraxisURL}/items/${item.id}`); });
     }
   });
-  sleep(1000);
-  it('Empty List', async () => {
-    const response = await agent.get(`${PraxisURL}/items`);
-
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.lengthOf(0);
+  afterEach(async () => {
+    const items = await agent.get(`${PraxisURL}/items`);
+    if (items.body.length > 0) {
+      items.body.forEach(async (item) => { await agent.delete(`${PraxisURL}/items/${item.id}`); });
+    }
   });
+  
   it('List with 1 product', async () => {
     const item = {
       name: 'Sugar',
@@ -45,4 +49,12 @@ describe('Test List Items Endpoint', () => {
     expect(lastItem.type).to.be.equal(item.type);
     expect(lastItem).to.be.jsonSchema(itemSchema);
   });
+
+  it('Empty List', async () => {
+    const response = await agent.get(`${PraxisURL}/items`);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.lengthOf(0);
+  });
+  
 });
