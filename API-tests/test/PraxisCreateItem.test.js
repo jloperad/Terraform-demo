b75195a5c76fdbec80747d2b08e7ee65c5fec27f
jloperad <51926858+jloperad@users.noreply.github.com>
2022-05-31 20:43:32 -0500

    Update PraxisCreateItem.test.js

--- API-tests/test/PraxisCreateItem.test.js
+++ API-tests/test/PraxisCreateItem.test.js
@@ -11,6 +11,7 @@ const PraxisURL = 'http://localhost:8080/api';
 
 describe('Test Create Item Endpoint', () => {
   before(async () => {
+    this.enableTimeouts(false);
     const items = await agent.get(`${PraxisURL}/items`);
     if (items.body.length > 0) {
       items.body.forEach(async (item) => { await agent.delete(`${PraxisURL}/items/${item.id}`); });
