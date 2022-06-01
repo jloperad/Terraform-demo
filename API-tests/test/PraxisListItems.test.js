4ab6518a16067235372e6ec9c37c2313ece62b0b
jloperad <51926858+jloperad@users.noreply.github.com>
2022-06-01 12:56:12 -0500

    Update PraxisListItems.test.js

--- API-tests/test/PraxisListItems.test.js
+++ API-tests/test/PraxisListItems.test.js
@@ -32,7 +32,7 @@ describe('Test List Items Endpoint', () => {
 
     const newItem = await agent.post(`${PraxisURL}/items`).send(item);
     const itemId = newItem.body.id;
-    const response = await agent.get('http://localhost:8080/api/items');
+    const response = await agent.get('http://54.162.70.124:8081/api/items');
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.lengthOf(1);
